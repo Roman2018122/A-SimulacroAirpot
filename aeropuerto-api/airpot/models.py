a44514e5bc0cd7944119f1bbfa0fad8896e5143d
Roman2018122 <jonathantorres1118@gmail.com>
2026-08-16 08:26:33 -0500

    Fist commit modelo y migrations

---
+++ aeropuerto-api/airpot/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Gate(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    terminal = models.CharField(max_length=20)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.code
+
+
+class Status(models.TextChoices):
+        SCHEDULED = "scheduled", "Scheduled"
+        BOARDING = "boarding", "Boarding"
+        DEPARTED = "departed", "Departed"
+        DELAYED = "delayed", "Delayed"
+        CANCELLED = "cancelled", "Cancelled"
+
+    
+class Flight(models.Model):
+    gate_id = models.ForeignKey(Gate, on_delete=models.PROTECT, related_name="flights")
+    flight_number = models.CharField(max_length=20)
+    destination = models.CharField(max_length=100)
+    status = models.CharField(
+            max_length=40,
+            choices=Status.choices,
+            default=Status.SCHEDULED
+        )
+    departure_time = models.DateTimeField()
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.gate_id.code} {self.flight_number} ({self.destination})"
